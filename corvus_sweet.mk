702f219c404276b112ce801311ccadd658d5cc4d
ajitlenka30 <ajitlenka30@gmail.com>
2023-06-24 22:00:24 +0530

    corvus changes

---
+++ corvus_sweet.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from sweet device
+$(call inherit-product, device/xiaomi/sweet/device.mk)
+
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+RAVEN_LAIR=Official
+CORVUS_MAINTAINER=Ajit
+USE_GAPPS=true
+USE_CUSTOM_CLANG=true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := corvus_sweet
+PRODUCT_DEVICE := sweet
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 10 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
